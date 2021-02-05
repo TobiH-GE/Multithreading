2a78b70a9b51be5b65ee36f086fe0379885ccd9c
TobiH-GE <tobias.hartwich@hotmail.de>
2021-02-05 23:25:12 +0100

    remove last commit

--- CalculationWithMultiThreads/MainWindow.xaml.cs
+++ CalculationWithMultiThreads/MainWindow.xaml.cs
@@ -114,9 +114,9 @@ namespace CalculationWithMultiThreads
             {
                 while (workers.Count <= _threads && counter < ListWithSegments.Count) //  - workers.Count
                 {
-                    Progress<int> pID = progressComs[counter % _threads];
-                    ArraySegment<Int64> workSegment = ListWithSegments[counter++];
-                    workers.Add(Task<Int64>.Run(() => Calc(workSegment, pID, cancelTokenSource.Token)));
+                    int pID = counter % _threads;
+                    int j = counter++;
+                    workers.Add(Task<Int64>.Run(() => Calc(ListWithSegments[j], progressComs[pID], cancelTokenSource.Token)));
                 }
                 Task<Int64> finishedTask = await Task.WhenAny(workers);
                 result += finishedTask.Result;
